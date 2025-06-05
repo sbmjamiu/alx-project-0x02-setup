01912d6475a61c88998a3e3815f90630698d8819
Jamiu Abdulhakeem <sbmjamiu@gmail.com>
2025-06-05 16:48:40 +0100

    home and about page added

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About: React.FC = () => (
+  <div>
+    <h1>About Page</h1>
+    <p>Welcome to the About page of our project.</p>
+  </div>
+);
+
+export default About;
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,8 @@
+const Home: React.FC = () => (
+  <div>
+    <h1>Home Page</h1>
+    <p>Welcome to the Home page of our project.</p>
+  </div>
+);
+
+export default Home;
